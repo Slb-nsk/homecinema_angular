40acc5d871a19cb6e1c2ee8ede2ef869d1492d72
Slb-nsk <wusong@ngs.ru>
2020-06-05 19:53:29 +0700

    refreshing

--- src/app/layout/create.component.ts
+++ src/app/layout/create.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
 import { Bigmovie } from './bigmovie.component';
 import { HttpService } from '../http.service'
-  
+
 @Component({
     selector: 'create-app',
     styles: [`
@@ -17,17 +17,17 @@ import { HttpService } from '../http.service'
 export class CreateComponent {
   movieRussianName: string = '';
   movieOriginalName: string = '';
-  seriesAmount:number = 1;
-  country:string[] = [];
-  genre: string[] = [];
-
+  seriesAmount: number = 1;
+  countries: string = '';
+  genres: string = '';
+  imageUrl: string = '';
 
   createForm: FormGroup;
 
 
 
-  constructor(private activateRoute: ActivatedRoute, 
-              private HttpService: HttpService, 
+  constructor(private activateRoute: ActivatedRoute,
+              private HttpService: HttpService,
               private router: Router){
     this.createForm = new FormGroup({
          "movieRussianName": new FormControl("", Validators.required),
@@ -35,31 +35,22 @@ export class CreateComponent {
          "movieYear": new FormControl(),
          "seriesAmount": new FormControl(),
          "description": new FormControl(),
-         "country": new FormArray([
-             new FormControl()
-           ]),
-         "genre": new FormArray([
-             new FormControl()
-           ])
+         "imageUrl": new FormControl(),
+         "countries": new FormControl(),
+         "genres": new FormControl()
         });
-         
-    } 
 
-    addCountry(){
-        (<FormArray>this.createForm.controls["country"]).push(new FormControl());
     }
 
-    addGenre(){
-        (<FormArray>this.createForm.controls["genre"]).push(new FormControl());
-    }
 
   onSubmit() {
-      this.HttpService.postData(this.createForm.value.movieRussianName, 
-                                this.createForm.value.movieOriginalName, 
+      this.HttpService.postData(this.createForm.value.movieRussianName,
+                                this.createForm.value.movieOriginalName,
                                 this.createForm.value.movieYear,
-                                this.createForm.value.seriesAmount, 
-                                this.createForm.value.country, 
-                                this.createForm.value.genre, 
+                                this.createForm.value.seriesAmount,
+                                this.createForm.value.countries,
+                                this.createForm.value.genres,
+                                this.createForm.value.imageUrl,
                                 this.createForm.value.description).subscribe();
       this.router.navigate(['ok'], {queryParams:{'name': this.createForm.value.movieRussianName}});
     }
--- src/app/layout/update.component.ts
+++ src/app/layout/update.component.ts
@@ -5,7 +5,7 @@ import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
 import { Bigmovie } from './bigmovie.component';
 import { HttpService } from '../http.service'
-  
+
 @Component({
     selector: 'update-app',
     templateUrl: './update.component.html',
@@ -14,7 +14,7 @@ import { HttpService } from '../http.service'
 export class UpdateComponent {
 
    private querySubscription: Subscription;
-   private movie: Bigmovie;      
+   private movie: Bigmovie;
       updateForm: FormGroup;
 
    constructor(private route: ActivatedRoute,
@@ -25,13 +25,14 @@ export class UpdateComponent {
       this.movie.movieId = this.route.snapshot.params['movieId'];
           this.querySubscription = this.route.queryParams.subscribe(
                 (queryParam: any) => {
-this.movie.movieRussianName= queryParam['movieRussianName']; 
-this.movie.movieOriginalName=queryParam['movieOriginalName']; 
-this.movie.movieYear= queryParam['movieYear'];
-this.movie.seriesAmount= queryParam['seriesAmount']; 
-this.movie.country= queryParam['country']; 
-this.movie.genre= queryParam['genre']; 
-this.movie.description= queryParam['description'];
+                  this.movie.movieRussianName= queryParam['movieRussianName'];
+                  this.movie.movieOriginalName=queryParam['movieOriginalName'];
+                  this.movie.movieYear= queryParam['movieYear'];
+                  this.movie.seriesAmount= queryParam['seriesAmount'];
+                  this.movie.countries= queryParam['countries'];
+                  this.movie.genres= queryParam['genres'];
+                  this.movie.description= queryParam['description'];
+                  this.movie.imageUrl= queryParam['imageUrl'];
                 }
           )
 
@@ -41,32 +42,13 @@ this.movie.description= queryParam['description'];
          "movieYear": new FormControl(this.movie.movieYear),
          "seriesAmount": new FormControl(this.movie.seriesAmount),
          "description": new FormControl(this.movie.description),
-         "country": new FormArray([
-             new FormControl()
-           ]),
-         "genre": new FormArray([
-             new FormControl()
-           ])
+         "genres": new FormControl(this.movie.genres),
+         "countries": new FormControl(this.movie.countries),
+         "imageUrl": new FormControl(this.movie.imageUrl)
         });
 
 
      }
-
-    addCountry(){
-        (<FormArray>this.updateForm.controls["country"]).push(new FormControl());
-    }
-
-    removeCountry(i: number){
-        (<FormArray>this.updateForm.controls["country"]).removeAt(i);
-    }
-
-    addGenre(){
-        (<FormArray>this.updateForm.controls["genre"]).push(new FormControl());
-    }
-
-    removeGenre(i: number){
-        (<FormArray>this.updateForm.controls["genre"]).removeAt(i);
-    }
 
     deleteAll(){
 console.log("delete movie with this ID:",this.movie.movieId);
@@ -86,12 +68,13 @@ console.log("delete movie with this ID:",this.movie.movieId);
 
   onSubmit() {
       this.HttpService.putData(this.movie.movieId,
-                                this.updateForm.value.movieRussianName, 
-                                this.updateForm.value.movieOriginalName, 
+                                this.updateForm.value.movieRussianName,
+                                this.updateForm.value.movieOriginalName,
                                 this.updateForm.value.movieYear,
-                                this.updateForm.value.seriesAmount, 
-                                this.updateForm.value.country, 
-                                this.updateForm.value.genre, 
+                                this.updateForm.value.seriesAmount,
+                                this.updateForm.value.countries,
+                                this.updateForm.value.genres,
+                                this.updateForm.value.imageUrl,
                                 this.updateForm.value.description).subscribe();
       this.router.navigate(['ok'], {queryParams:{'name': this.updateForm.value.movieRussianName}});
     }
@@ -100,7 +83,7 @@ console.log("delete movie with this ID:",this.movie.movieId);
 
   ngOnDestroy() { this.querySubscription.unsubscribe();}
 
-     
+
 }
 
 
